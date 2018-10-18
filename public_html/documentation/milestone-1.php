1630ed82da29ddf8fd83d32e41a9c700524e9f2d
Sharon Romero <sh-aron@hotmail.com>
2018-10-17 20:08:05 -0600

    Added purpose, audience, and goal.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Sharon Romero Gallery</title>
+	</head>
+
+	<p><strong>Purpose:</strong>The purpose of this website is to to allow fellow web developers and designers to collaborate and keep in touch with me.</p>
+
+	<p><strong>Audience:</strong>This website is geared toward fellow web designers and developers who are just starting out in the field and who want to take a look at my projects.</p>
+
+	<p><strong>Goal:</strong>The goal of this website is to serve as a gallery to showcase and promote my work and to help in generating ideas and inspiration between web design and developer colleagues and myself.</p>
+
+</html>
