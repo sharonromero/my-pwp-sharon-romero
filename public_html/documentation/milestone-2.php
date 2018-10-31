5cd44457e0459c730790152359a825dcb0b1f0c6
sharonromero <sh-aron@hotmail.com>
2018-10-31 16:08:17 -0600

    Completed information about the content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,9 +6,9 @@
 	</head>
 
 	<body>
-		<p><strong>Title:</strong>The title area will list the name of the website. It will also contain links to the About and Content sections of the page.</p>
-		<p><strong>Gallery:</strong>This area will show a gallery of the projects that I have worked on.</p>
-		<p><strong>About:</strong>This area will have a description of my background along with a short description of the purpose of the website.</p>
-		<p></p>
+		<p><strong>Title</strong>: The title area will list the name of the website. It will also contain links to the About and Content sections of the page.</p>
+		<p><strong>Gallery</strong>: This area will show a gallery of the projects that I have worked on.</p>
+		<p><strong>About</strong>: This area will have a description of my background along with a short description of the purpose of the website.</p>
+		<p><strong>Contact Form</strong>: This section will have a contact form that visitors can fill out with their questions or comments.</p>
 
 	</body>
