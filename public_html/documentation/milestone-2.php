d1d0a0e43139595a1c00422b77800b636fb749cd
sharonromero <sh-aron@hotmail.com>
2018-10-31 14:28:12 -0600

    Added desktop wireframe and initial information about the content strategy for the webpage.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Content Strategy</title>
+	</head>
+
+	<body>
+		<p><strong>Title:</strong>The title area will list the name of the website. It will also contain links to the About and Content sections of the page.</p>
+		<p><strong>Gallery:</strong>This area will show a gallery of the projects that I have worked on.</p>
+		<p><strong>About:</strong>This area will have a description of my background along with a short description of the purpose of the website.</p>
+		<p></p>
+
+	</body>
