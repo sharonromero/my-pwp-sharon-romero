dc08902b192160981c8f48993df6af472b7ad00d
sharonromero <sh-aron@hotmail.com>
2018-11-28 21:04:48 -0700

    Cleaned up spacing

--- public_html/php/mailer.php
+++ public_html/php/mailer.php
@@ -23,31 +23,38 @@ try {
 
 		//if there's a reCAPTCHA error, throw an exception
 	if (!$resp->isSuccess()) {
-		throw(new Exception("reCAPTCHA error!"));
+				throw(new Exception("reCAPTCHA error!"));
 	}
+
 	/**
 	 * Sanitize the inputs from the form: name, email, subject, and message.
 	 * This assumes jQuery (NOT Angulat!) will be AJAX submitting the form,
-	 * so we're using the $_POST superglobal
+	 * so we're using the $_POST superglobal.
 	 **/
+
 	$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 	$message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
 	// create Swift message
 	$swiftMessage = new Swift_Message();
+
 	/**
 	 * Attach the sender to the message.
 	 * This takes the form of an associative array where $email is the key for the real name.
 	 **/
 	$swiftMessage->setFrom([$email => $name]);
+
 	/**
 	 * Attach the recipients to the message.
 	 * $MAIL_RECIPIENTS is set in mail-config.php
 	 **/
 	$recipients = $MAIL_RECIPIENTS;
 	$swiftMessage->setTo($recipients);
+
 	// attach the subject line to the message
 	$swiftMessage->setSubject($subject);
+
 	/**
 	 * Attach the actual message to the message.
 	 *
@@ -61,6 +68,7 @@ try {
 	 **/
 	$swiftMessage->setBody($message, "text/html");
 	$swiftMessage->addPart(html_entity_decode($message), "text/plain");
+
 	/**
 	 * Send the Email via SMTP. The SMTP server here is configured to relay
 	 * everything upstream via your web host.
@@ -80,16 +88,19 @@ try {
 		->setPassword($smtpSecret);
 	$mailer = new Swift_Mailer($smtp);
 	$numSent = $mailer->send($swiftMessage, $failedRecipients);
+
 	/**
 	 * The send method returns the number of recipients that accepted the Email.
 	 * If the number attempted !== number accepted it's an Exception.
 	 **/
 	if($numSent !== count($recipients)) {
-		// The $failedRecipients parameter passed in the send() contains an array of the Emails that failed.
-		throw(new RuntimeException("unable to send email"));
+			// The $failedRecipients parameter passed in the send() contains an array of the Emails that failed.
+			throw(new RuntimeException("unable to send email"));
 	}
+
 	// report a successful send!
 	echo "<div class=\"alert alert-success\" role=\"alert\">Email successfully sent.</div>";
+
 } catch(Exception $exception) {
 	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send email: " . $exception->getMessage() . "</div>";
 }
